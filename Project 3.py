273268cad398dfdc0da56d0069f19d4afa6ba50c
Aniv Chakravarty <anivchakravarty@gmail.com>
2021-11-06 15:50:07 -0500

    seems to be working but not sure

--- Project 3.py
+++ Project 3.py
@@ -10,10 +10,10 @@ add display for alpha and beta values considered at each step at bottom/top'''
 '''imports'''
 import pygame,sys
 import numpy as np
+import random
 '''Initializing window'''
 pygame.init()
-WIDTH = 600
-HEIGHT = 600
+WIDTH, HEIGHT = 600, 600
 COLOR=(220,220,220)
 BLACK=(0,0,0)
 TRANSPARENT=(0,0,0,0)
@@ -21,15 +21,14 @@ GREEN=(0,250,0)
 CIRCLE=(128,0,128)
 CROSS = (0,0,250)
 LINE_WIDTH=10
-ROWS=3
-COLUMNS=3
+ROWS, COLUMNS = 3,3
 player=1
 max_stats=[0,0,0]
 min_stats=[0,0,0]
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("5210 Project 3: Tic Tac Toe")
-alpha_beta=[1000,-1000]
-util_score={'max':1,'min':-1,'tie':0}
+alpha_beta=[-1000, 1000]
+util_score={'max':1 ,'min':-1 ,'tie':0}
 score=0
 X,Y=1,1
 X1,Y1,X2,Y2,X3,Y3,X4,Y4=258,208,343,208,343,292,258,292
@@ -41,7 +40,7 @@ game_start=False
 board=np.zeros((ROWS,COLUMNS))
 #print(board)
 def text():
-    font = pygame.font.Font('freesansbold.ttf', 20)
+    font = pygame.font.Font('freesansbold.ttf', 15)
     if player==1:
         t = ' Player Max\'s turn '
     elif player==2:
@@ -51,19 +50,27 @@ def text():
         'Player Max Win:{} Loss:{} Draw:{}'.format(max_stats[0], max_stats[1], max_stats[2]), True, BLACK, COLOR)
     text3 = font.render(
         'Player Min Win:{} Loss:{} Draw:{}'.format(min_stats[0], min_stats[1], min_stats[2]), True, BLACK, COLOR)
-    text4 = font.render(" Press S to start. Press R to reset. Use arrow keys to move and Enter key to select cell", True, BLACK, COLOR)
+    text4 = font.render(" Press S to start. Press R to reset. Use arrow keys to move and Enter key to select.", True, BLACK, COLOR)
+    text5 = font.render("Alpha:{}".format(alpha_beta[0]), True, BLACK, COLOR)
+    text6= font.render("Beta:{}".format(alpha_beta[1]), True, BLACK, COLOR)
     textbox1 = text1.get_rect()
     textbox2 = text2.get_rect()
     textbox3 = text3.get_rect()
     textbox4 = text4.get_rect()
+    textbox5 = text5.get_rect()
+    textbox6 = text6.get_rect()
     textbox1.center = (300, 420)
-    textbox2.center = (160, 20)
+    textbox2.center = (158, 20)
     textbox3.center = (156, 40)
-    textbox4.center = (300, 520)
+    textbox4.center = (300, 450)
+    textbox5.center = (500,20)
+    textbox6.center = (500,40)
     window.blit(text1, textbox1)
     window.blit(text2, textbox2)
     window.blit(text3, textbox3)
     window.blit(text4, textbox4)
+    window.blit(text5, textbox5)
+    window.blit(text6, textbox6)
     return
 def highlight(x1,y1,x2,y2,x3,y3,x4,y4):
     pygame.draw.line(window, GREEN, (x1, y1), (x2, y2), 5)
@@ -73,16 +80,135 @@ def highlight(x1,y1,x2,y2,x3,y3,x4,y4):
     return
 def mark(row, col, player):
     board[row][col] = player
-#mark(0,0,1)
 print(board)
 
 def full():
     for row in range(ROWS):
         for col in range(COLUMNS):
-            if board[row][col]==0:
+            if free(row,col):
                 return False
     return True
+#alpha beta pruning decision
+#get this working using minimax
+def AlphaBeta_Search(board):
+    alpha=-1000
+    beta=1000
+    best=alpha
+    for row in range(ROWS):
+        for col in range(COLUMNS):
+            if free(row,col):
+                board[row][col]=1
+                score = ab(board, 0, False, alpha, beta)
+                print('alpha:{}, beta:{}'.format(alpha,beta))
+                board[row][col]=0
+                if score>best:
+                    best = score
+                    move = (row,col)
+    return move[0],move[1]
 
+def ab(board, depth, isMax, alpha, beta):
+    if goal(1):
+        return 1
+    elif goal(2):
+        return -1
+    elif full():
+        return 0
+    if isMax:
+        best = alpha
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if free(row,col):
+                    board[row][col]=1
+                    score = ab(board, depth+1, False, alpha, beta)
+                    print('alpha:{}, beta:{}'.format(alpha, beta))
+                    board[row][col]=0
+                    best=max(score,best)
+                    alpha=best
+                    alpha_beta[0]=alpha
+                    if best >= beta:
+                        return best
+        return best
+    else:
+        best = beta
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if free(row,col):
+                    board[row][col]=2
+                    score = ab(board, depth+1, True, alpha, beta)
+                    print('alpha:{}, beta:{}'.format(alpha, beta))
+                    board[row][col]=0
+                    best=min(score,best)
+                    beta=best
+                    alpha_beta[1]=beta
+                    if best <= alpha:
+                        return best
+        return best
+
+'''def AlphaBeta_Search(board):
+    actions=[]
+    o=[]
+    alpha=-1000
+    best=-1000
+    beta=1000
+    for row in range(ROWS):
+        for col in range(COLUMNS):
+            if free(row,col):
+                a=(row,col)
+                board[row][col]=1
+                score, alpha, beta = Min(board, a, alpha, beta)
+                board[row][col]=0
+                if score > best:
+                    best, move = score, a
+                option=(score, move, alpha, beta)
+                actions.append(option)
+                print('move:{}'.format(move))
+    print(actions)
+    refresh()
+    return move[0],move[1]
+def Max(board, move, alpha, beta):
+    if goal(1):
+        return 1, alpha, beta
+    elif goal(2):
+        return -1, alpha, beta
+    elif full():
+        return 0, alpha, beta
+    v=alpha
+    for x1 in range(ROWS):
+        for y1 in range(COLUMNS):
+            if free(x1,y1):
+                a=(x1,y1)
+                board[x1][y1]=1
+                v2, alpha, beta = Min(board, a, alpha, beta)
+                board[x1][y1]=0
+                if v2 > v:
+                    v, move = v2, a
+                    alpha=max(alpha,v)
+                    alpha_beta[0]=alpha
+                if v >= beta:
+                    return v, alpha, beta
+    return v, alpha, beta
+def Min(board, move, alpha, beta):
+    if goal(2):
+        return -1, alpha, beta
+    elif goal(1):
+        return 1, alpha, beta
+    elif full():
+        return 0, alpha, beta
+    v=beta
+    for x1 in range(ROWS):
+        for y1 in range(COLUMNS):
+            if free(x1,y1):
+                a=(x1,y1)
+                board[x1][y1]=2
+                v2, alpha, beta = Max(board, a, alpha, beta)
+                board[x1][y1]=0
+                if v2 < v:
+                    v, move = v2, a
+                    beta=min(beta,v)
+                    alpha_beta[1]=beta
+                if v <= alpha:
+                    return v, alpha, beta
+    return v, alpha, beta'''
 def draw():
     for row in range(ROWS):
         for col in range(COLUMNS):
@@ -91,7 +217,7 @@ def draw():
             if board[row][col]==1:
                 pygame.draw.line( window, CROSS, (col * 100 + 240, row * 100 + 120), (col*100+160, row * 100 + 180), 10)
                 pygame.draw.line(window, CROSS, (col * 100 + 160, row * 100 + 120), (col*100+240, row * 100 + 180), 10)
-
+#optimize if else
 #available
 def free(row,col):
     return board[row][col]==0
@@ -170,57 +296,78 @@ def restart(board):
     player=1
     game_end=False
     game_start=False
+    alpha_beta=[-1000,1000]
     draw()
     return game_end,game_start,board,player
+def move(player, x, y, game_end, game_start):
+    if player == 1:
+        x, y = AlphaBeta_Search(board)
+        mark(x, y, 1)
+        refresh()
+        print(board)
+        if goal(player):
+            game_end = True
+            game_start = False
+            max_stats[0] += 1
+            min_stats[1] += 1
+            return game_end, game_start, player
+        elif full():
+            max_stats[2] += 1
+            min_stats[2] += 1
+        else:
+            player=2
+    elif player == 2:
+        mark(x, y, 2)
+        refresh()
+        if goal(player):
+            game_end = True
+            game_start = False
+            max_stats[1] += 1
+            min_stats[0] += 1
+            return game_end, game_start, player
+        elif full():
+            max_stats[2] += 1
+            min_stats[2] += 1
+        else:
+            player=1
+    return game_end, game_start, player
+def refresh():
+    window.fill(COLOR)
+    grid()
+    draw()
 #display loop
+
 while True:
     grid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    #link board
+        #link board
         '''key movement'''
-        #highlight('center')
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s and not game_start:
                 game_start=True
+                    #x,y=0,0
                 x1,y1,x2,y2,x3,y3,x4,y4=X1,Y1,X2,Y2,X3,Y3,X4,Y4
                 x,y=X,Y
+                game_end, game_start, player = move(player, x, y, game_end, game_start)
+                refresh()
                 highlight(x1, y1, x2, y2, x3, y3, x4, y4)
             '''navigation working enter player change bugged'''
             if event.key == pygame.K_RETURN and not game_end and game_start:
                 print(x,y)
                 if free(x,y):
-                    if player==1:
-                        mark(x,y,1)
-                        print(board)
-                        if goal(player):
-                            game_end = True
-                            game_start=False
-                            max_stats[0] += 1
-                            min_stats[1] += 1
-                        elif full():
-                            max_stats[2]+=1
-                            min_stats[2]+=1
-                        player = 2
-                    elif player==2:
-                        mark(x, y, 2)
-                        if goal(player):
-                            game_end = True
-                            game_start=False
-                            max_stats[0] += 1
-                            min_stats[1] += 1
-                        elif full():
-                            max_stats[2] += 1
-                            min_stats[2] += 1
-                        player = 1
-                    draw()
+                    if player==2:
+                        game_end, game_start, player = move(player, x, y, game_end, game_start)
+                        #refresh()
+                        if not game_end:
+                            game_end, game_start, player = move(player, x, y, game_end, game_start)
+                            highlight(x1, y1, x2, y2, x3, y3, x4, y4)
+
             elif event.key == pygame.K_LEFT and game_start:
                 if y>0:
                     y -= 1
-                    window.fill(COLOR)
-                    grid()
-                    draw()
+                    refresh()
                     x1-=100
                     x2-=100
                     x3-=100
@@ -230,9 +377,7 @@ while True:
             elif event.key == pygame.K_RIGHT and game_start:
                 if y<2:
                     y +=1
-                    window.fill(COLOR)
-                    grid()
-                    draw()
+                    refresh()
                     x1+=100
                     x2+=100
                     x3+=100
@@ -242,9 +387,7 @@ while True:
             elif event.key == pygame.K_UP and game_start:
                 if x>0:
                     x -=1
-                    window.fill(COLOR)
-                    grid()
-                    draw()
+                    refresh()
                     y1-=100
                     y2-=100
                     y3-=100
@@ -254,9 +397,7 @@ while True:
             elif event.key == pygame.K_DOWN and game_start:
                 if x<2:
                     x +=1
-                    window.fill(COLOR)
-                    grid()
-                    draw()
+                    refresh()
                     y1+=100
                     y2+=100
                     y3+=100
@@ -265,35 +406,6 @@ while True:
                     #print(x,y)
             if event.key == pygame.K_r:
                 game_end, game_start, board, player = restart(board)
-
-        '''currently with mouse click, convert this to navigation for player 2 with arrow keys and enter key like refresh'''
-        '''if event.type == pygame.MOUSEBUTTONDOWN and not game_end:
-            mouseX=event.pos[0]#x
-            mouseY=event.pos[1]#y
-            click_row= int((mouseY-100)//100)
-            click_col= int((mouseX-150)//100)
-
-            if (click_row < 0 or click_row > 2 ) or (click_col < 0 or click_col > 2):
-                continue
-            elif free(click_row, click_col):
-                if player==1:
-                    mark(click_row,click_col,1)
-                    if goal(player):
-                        game_end=True
-                        max_stats[0]+=1
-                        min_stats[1]+=1
-                    player=2
-                elif player==2:
-                    mark(click_row,click_col,2)
-                    if goal(player):
-                        game_end=True
-                        max_stats[0]+=1
-                        min_stats[1]+=1
-                    player=1
-        if full():
-            max_stats[2]+=1
-            min_stats[2]+=1
-            draw()'''
         text()
         #highlight(0)
         #if event.type == pygame.KEYDOWN:
