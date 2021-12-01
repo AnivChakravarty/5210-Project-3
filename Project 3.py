508caf50cd3273f60fcf63e7e11a921d2c6743e9
Aniv Chakravarty <anivchakravarty@gmail.com>
2021-12-01 13:55:52 -0600

    Final

--- Project 3.py
+++ Project 3.py
@@ -1,19 +1,18 @@
-#https://www.geeksforgeeks.org/tic-tac-toe-gui-in-python-using-pygame/
-#https://www.youtube.com/watch?v=pc7XhHxSgrM&ab_channel=CodingSpot
-#https://www.youtube.com/watch?v=fT3YWCKvuQE&ab_channel=KylieYing
-#https://www.youtube.com/watch?v=trKjYdBASyQ&ab_channel=TheCodingTrain
-'''TO DO:
-2 player tic tac toe shows a line on 3, currently set to on click which needs to be changed to keys and highlight which cell is being filled.
-Display text at the top such as win/loss/draw
-add button for refresh at the bottom,
-add display for alpha and beta values considered at each step at bottom/top'''
+'''
+References
+https://www.geeksforgeeks.org/tic-tac-toe-gui-in-python-using-pygame/
+https://www.youtube.com/watch?v=pc7XhHxSgrM&ab_channel=CodingSpot
+https://www.youtube.com/watch?v=fT3YWCKvuQE&ab_channel=KylieYing
+https://www.youtube.com/watch?v=trKjYdBASyQ&ab_channel=TheCodingTrain
+'''
+
 '''imports'''
 import pygame,sys
 import numpy as np
+import timeit
 import random
 '''Initializing window'''
 pygame.init()
-#window dimensions
 WIDTH, HEIGHT = 600, 600
 COLOR=(220,220,220)
 BLACK=(0,0,0)
@@ -29,17 +28,17 @@ min_stats=[0,0,0]
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("5210 Project 3: Tic Tac Toe")
 alpha_beta=[-1000, 1000]
-util_score={'max':1 ,'min':-1 ,'tie':0}
+count=[]
+time=[]
+prune=[]
 score=0
 X,Y=1,1
 X1,Y1,X2,Y2,X3,Y3,X4,Y4=258,208,343,208,343,292,258,292
 window.fill(COLOR)
 game_end=False
 game_start=False
-#board
-'''environment is 2d matrix board'''
+'''Setting up the environment as a 2d matrix board with default value 0. Player 1 fills 1, player 2 fills 2'''
 board=np.zeros((ROWS,COLUMNS))
-#print(board)
 def text():
     font = pygame.font.Font('freesansbold.ttf', 10)
     if player==1:
@@ -60,14 +59,14 @@ def text():
     textbox4 = text4.get_rect()
     textbox5 = text5.get_rect()
     textbox6 = text6.get_rect()
-    #bottom turn text
+    '''Turn text below grid'''
     textbox1.center = (300, 420)
-    #players top left
+    '''keeping track of player1 and player2 scores at the top left'''
     textbox2.center = (158, 20)
     textbox3.center = (156, 40)
-    #bottom text
+    '''keyboard instructions below the grid'''
     textbox4.center = (300, 450)
-    #alpha-beta text
+    '''Display text for Alpha and Beta values on the top right'''
     textbox5.center = (500,20)
     textbox6.center = (500,40)
     window.blit(text1, textbox1)
@@ -77,6 +76,7 @@ def text():
     window.blit(text5, textbox5)
     window.blit(text6, textbox6)
     return
+'''green highlight box'''
 def highlight(x1,y1,x2,y2,x3,y3,x4,y4):
     pygame.draw.line(window, GREEN, (x1, y1), (x2, y2), 5)
     pygame.draw.line(window, GREEN, (x2, y2), (x3, y3), 5)
@@ -86,141 +86,162 @@ def highlight(x1,y1,x2,y2,x3,y3,x4,y4):
 def mark(row, col, player):
     board[row][col] = player
 print(board)
-
+'''tie condition or board is full'''
 def full():
     for row in range(ROWS):
         for col in range(COLUMNS):
             if free(row,col):
                 return False
     return True
-#alpha beta pruning decision
-#working, now to add depth cutoff
+'''Search methods'''
 def AlphaBeta_Search(board):
     alpha=-1000
     beta=1000
-    best=alpha
+    maxbest=alpha
     depth=0
-    cutoff=0
-    actions=[]
-    #for cutoff in range(9):
-    for row in range(ROWS):
-        for col in range(COLUMNS):
-            if free(row,col):
-                board[row][col]=1
-                score = ab(board, depth+1, False, alpha, beta, cutoff)
-                print('x:{}, y:{}, s:{}, alpha:{}, beta:{}'.format(row, col, score, alpha, beta))
-                board[row][col]=0
-                if score>best:
-                    best = score
-                    alpha_beta[0]=best
-                    move = (row,col)
+    c=0
+    original_stdout=sys.stdout
+    '''write to log file'''
+    with open('logPruning2.txt', 'a') as f:
+        sys.stdout=f
+        begin=timeit.timeit()
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if free(row,col):
+                    c+=1
+                    board[row][col]=1
+                    score,c = ab(board, depth+1, False, alpha, beta,c)
+                    #print('xmax:{}, ymax:{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, score, alpha, beta, depth))
+                    board[row][col]=0
+                    if score>maxbest:
+                        maxbest = score
+                        alpha = score
+                        alpha_beta[0]=score
+                        move = (row,col)
+                    if score>=beta:
+                        prune.append((row,col,alpha,beta))
+                        #print('pruned at {},{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, score, alpha, beta, depth))
+                        break
+        end=timeit.timeit()
+        time.append(end-begin)
+        count.append(c)
+        print('total nodes:{}'.format(sum(count)))
+        print('total method time:{}'.format(sum(time)))
+        print('number of prunes:{}'.format(len(prune)))
+        sys.stdout=original_stdout
     return move[0],move[1]
-
-def ab(board, depth, isMax, alpha, beta, cutoff):
+'''Recursive call for aplpha beta pruning'''
+def ab(board, depth, isMax, alpha, beta,c):
     if goal(1):
-        return 1
+        return 1, c
     elif goal(2):
-        return -1
+        return -1, c
     elif full():
-        return 0
+        return 0, c
     if isMax:
         best = alpha
         for row in range(ROWS):
             for col in range(COLUMNS):
                 if free(row,col):
+                    c+=1
                     board[row][col]=1
-                    score = ab(board, depth+1, False, alpha, beta, cutoff)
-                    print('x2:{}, y2:{}, s:{}, alpha:{}, beta:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1]))
+                    score,c = ab(board, depth+1, False, alpha, beta,c)
+                    #print('x2:{}, y2:{}, s:{}, alpha:{}, beta:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1]))
                     board[row][col]=0
                     if score > best:
                         best=score
                         alpha=best
                         alpha_beta[0]=best
-                    if best >= beta:
+                    #print('xmax:{}, ymax:{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1], depth))
+                    if score >= beta:
+                        #return best
+                        prune.append((row,col,alpha,beta))
+                        #print('pruned at {},{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, score, alpha, beta, depth))
                         break
-        return best
+        return best,c
     else:
         best = beta
         for row in range(ROWS):
             for col in range(COLUMNS):
                 if free(row,col):
+                    c+=1
                     board[row][col]=2
-                    score = ab(board, depth+1, True, alpha, beta, cutoff)
-                    print('x1:{}, y1:{}, s:{}, alpha:{}, beta:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1]))
+                    score,c = ab(board, depth+1, True, alpha, beta, c)
+                    #print('x1:{}, y1:{}, s:{}, alpha:{}, beta:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1]))
                     board[row][col]=0
                     if score < best:
                         best=score
                         beta=best
                         alpha_beta[1]=best
-                    if best <= alpha:
+                    #print('xmin:{}, ymin:{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1], depth))
+                    if score <= alpha:
+                        prune.append((row, col, alpha, beta))
+                        #print('pruned at {},{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, score, alpha, beta, depth))
                         break
-        return best
-
-'''def AlphaBeta_Search(board):
-    actions=[]
-    o=[]
-    alpha=-1000
-    best=-1000
-    beta=1000
-    for row in range(ROWS):
-        for col in range(COLUMNS):
-            if free(row,col):
-                a=(row,col)
-                board[row][col]=1
-                score, alpha, beta = Min(board, a, alpha, beta)
-                board[row][col]=0
-                if score > best:
-                    best, move = score, a
-                option=(score, move, alpha, beta)
-                actions.append(option)
-                print('move:{}'.format(move))
-    print(actions)
-    refresh()
+        return best,c
+'''standard minimax'''
+def Minimax_Search(board):
+    depth=0
+    c=0
+    '''write to log file'''
+    original_stdout=sys.stdout
+    with open('logMinimax2.txt', 'a') as f:
+        sys.stdout=f
+        best=-2
+        begin=timeit.timeit()
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if free(row,col):
+                    c+=1
+                    board[row][col]=1
+                    score,c = mm(board, depth+1, False,c)
+                    #print('xmax:{}, ymax:{}, s:{}, depth:{}'.format(row, col, score, depth))
+                    board[row][col]=0
+                    if score>best:
+                        best = score
+                        move = (row,col)
+        end=timeit.timeit()
+        time.append(end-begin)
+        count.append(c)
+        print('total nodes:{}'.format(sum(count)))
+        print('total method time:{}'.format(sum(time)))
+        sys.stdout=original_stdout
     return move[0],move[1]
-def Max(board, move, alpha, beta):
+'''Recursive call for minimax'''
+def mm(board, depth, isMax,c):
     if goal(1):
-        return 1, alpha, beta
+        return 1, c
     elif goal(2):
-        return -1, alpha, beta
+        return -1, c
     elif full():
-        return 0, alpha, beta
-    v=alpha
-    for x1 in range(ROWS):
-        for y1 in range(COLUMNS):
-            if free(x1,y1):
-                a=(x1,y1)
-                board[x1][y1]=1
-                v2, alpha, beta = Min(board, a, alpha, beta)
-                board[x1][y1]=0
-                if v2 > v:
-                    v, move = v2, a
-                    alpha=max(alpha,v)
-                    alpha_beta[0]=alpha
-                if v >= beta:
-                    return v, alpha, beta
-    return v, alpha, beta
-def Min(board, move, alpha, beta):
-    if goal(2):
-        return -1, alpha, beta
-    elif goal(1):
-        return 1, alpha, beta
-    elif full():
-        return 0, alpha, beta
-    v=beta
-    for x1 in range(ROWS):
-        for y1 in range(COLUMNS):
-            if free(x1,y1):
-                a=(x1,y1)
-                board[x1][y1]=2
-                v2, alpha, beta = Max(board, a, alpha, beta)
-                board[x1][y1]=0
-                if v2 < v:
-                    v, move = v2, a
-                    beta=min(beta,v)
-                    alpha_beta[1]=beta
-                if v <= alpha:
-                    return v, alpha, beta
-    return v, alpha, beta'''
+        return 0, c
+    if isMax:
+        best=-1000
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if free(row,col):
+                    c+=1
+                    board[row][col]=1
+                    score,c = mm(board, depth+1, False,c)
+                    #print('x2:{}, y2:{}, s:{}, alpha:{}, beta:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1]))
+                    board[row][col]=0
+                    best=max(best,score)
+                    #print('xmax:{}, ymax:{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, score, depth))
+        return best,c
+    else:
+        best = 1000
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if free(row,col):
+                    c+=1
+                    board[row][col]=2
+                    score,c = mm(board, depth+1, True,c)
+                    #print('x1:{}, y1:{}, s:{}, alpha:{}, beta:{}'.format(row, col, score, alpha_beta[0], alpha_beta[1]))
+                    board[row][col]=0
+                    best=min(score,best)
+                    #print('xmin:{}, ymin:{}, s:{}, alpha:{}, beta:{}, depth:{}'.format(row, col, best, depth))
+        return best,c
+
 def draw():
     for row in range(ROWS):
         for col in range(COLUMNS):
@@ -229,11 +250,10 @@ def draw():
             if board[row][col]==1:
                 pygame.draw.line( window, CROSS, (col * 100 + 240, row * 100 + 120), (col*100+160, row * 100 + 180), 10)
                 pygame.draw.line(window, CROSS, (col * 100 + 160, row * 100 + 120), (col*100+240, row * 100 + 180), 10)
-#optimize if else
-#available
+'''check for free cell'''
 def free(row,col):
     return board[row][col]==0
-
+'''check for goal conditions'''
 def goal(player):
     #col win
     for col in range(COLUMNS):
@@ -249,11 +269,12 @@ def goal(player):
     if board[0][0]==player and board[1][1]==player and board[2][2]==player:
         win_line_d1(player)
         return True
+    #diagonal 2
     if board[0][2]==player and board[1][1]==player and board[2][0]==player:
         win_line_d2(player)
         return True
     return False
-
+'''drawing lines between winning cells'''
 def win_line_col(col, player) :
     pX = col * 100 + 200
     color=(0,0,0)
@@ -276,44 +297,45 @@ def win_line_d1(player) :
     elif player == 2:
         color = CIRCLE
     pygame.draw.line(window, color, (170,120), (440,390), 15)
-
-
 def win_line_d2(player) :
     if player == 1:
         color = CROSS
     elif player == 2:
         color = CIRCLE
     pygame.draw.line(window, color, (420,120), (165,390), 15)
+'''Drawing interface Grid'''
 def grid():
-    #horizontal
+    '''horizontal'''
     pygame.draw.line(window, BLACK, (150, 200), (450, 200), LINE_WIDTH)
     pygame.draw.line(window, BLACK, (150, 300), (450, 300), LINE_WIDTH)
     pygame.draw.line(window, BLACK, (146, 100), (455, 100), LINE_WIDTH)
     pygame.draw.line(window, BLACK, (146, 400), (455, 400), LINE_WIDTH)
-    #vertical
+    '''vertical'''
     pygame.draw.line(window, BLACK, (250, 100), (250, 400), LINE_WIDTH)
     pygame.draw.line(window, BLACK, (350, 100), (350, 400), LINE_WIDTH)
     pygame.draw.line(window, BLACK, (150, 100), (150, 400), LINE_WIDTH)
     pygame.draw.line(window, BLACK, (450, 100), (450, 400), LINE_WIDTH)
-
-
+'''restart button resets all variables except score stats of player 1 and player 2'''
 def restart(board):
     window.fill(COLOR)
     grid()
-    #x1,y1,x2,y2,x3,y3,x4,y4=X1,Y1,X2,Y2,X3,Y3,X4,Y4
-    #highlight(x1,y1,x2,y2,x3,y3,x4,y4)
     for i in range(ROWS):
         for j in range(COLUMNS):
             board[i][j]=0
     player=1
     game_end=False
     game_start=False
-    alpha_beta=[-1000,1000]
+    count.clear()
+    time.clear()
+    prune.clear()
+    alpha_beta[0]=-1000
+    alpha_beta[1]=1000
     draw()
     return game_end,game_start,board,player
 def move(player, x, y, game_end, game_start):
     if player == 1:
         x, y = AlphaBeta_Search(board)
+        #x, y = Minimax_Search(board)
         mark(x, y, 1)
         refresh()
         print(board)
@@ -343,23 +365,22 @@ def move(player, x, y, game_end, game_start):
         else:
             player=1
     return game_end, game_start, player
+'''update grid with new position'''
 def refresh():
     window.fill(COLOR)
     grid()
     draw()
-#display loop
-
+'''display loop'''
 while True:
     grid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        #link board
+        '''link board to visual interface'''
         '''key movement'''
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s and not game_start:
                 game_start=True
-                    #x,y=0,0
                 x1,y1,x2,y2,x3,y3,x4,y4=X1,Y1,X2,Y2,X3,Y3,X4,Y4
                 x,y=X,Y
                 game_end, game_start, player = move(player, x, y, game_end, game_start)
@@ -375,7 +396,6 @@ while True:
                         if not game_end:
                             game_end, game_start, player = move(player, x, y, game_end, game_start)
                             highlight(x1, y1, x2, y2, x3, y3, x4, y4)
-
             elif event.key == pygame.K_LEFT and game_start:
                 if y>0:
                     y -= 1
@@ -415,12 +435,7 @@ while True:
                     y3+=100
                     y4+=100
                     highlight(x1, y1, x2, y2, x3, y3, x4, y4)
-                    #print(x,y)
             if event.key == pygame.K_r:
                 game_end, game_start, board, player = restart(board)
         text()
-        #highlight(0)
-        #if event.type == pygame.KEYDOWN:
-#    elif event.key == pygame.K_r:
-#        restart()
     pygame.display.update()
